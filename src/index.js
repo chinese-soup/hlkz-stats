e8af8e73fd0c8feb0a9496f0a798d4a8e5a3024a
execut4ble <jokubas.banaitis@gmail.com>
2021-06-10 22:01:37 +0300

    Use a service worker

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import MapLeaderboard from "./pages/mapLeaderboard.page";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import Navbar from "./components/navbar";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -37,3 +38,8 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
