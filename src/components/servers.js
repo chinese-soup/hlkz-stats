f3b6cf73e5c523c8448b728001a1cb26db17ca28
execut4ble <jokubas.banaitis@gmail.com>
2022-11-22 23:31:15 +0200

    Update servers list component

--- src/components/servers.js
+++ src/components/servers.js
@@ -20,9 +20,10 @@ const ServerList = () => {
       <Server country="uk" address="212.71.238.124:27015"></Server>
       <Server country="us" address="173.255.224.173:27015"></Server>
       <Server country="de" address="139.162.187.16:27015"></Server>
-      <Server country="fi" address="95.216.211.173:27015"></Server>
-      <Server country="de" address="78.46.191.68:27015" desc="100aa"></Server>
+      <Server country="fr" address="45.76.46.156:27016" desc="100fps"></Server>
+      <Server country="fr" address="45.76.46.156:27015"></Server>
       <Server country="nl" address="108.61.99.176:27016"></Server>
+      <Server country="de" address="80.240.26.43:27015"></Server>
     </ul>
   );
 };
