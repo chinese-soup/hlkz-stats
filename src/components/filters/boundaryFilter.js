e89def746a9c7eecb8f0cbdda253d6f368b9b535
execut4ble <jokubas.banaitis@gmail.com>
2023-01-20 19:10:06 +0200

    Make filter update on operator change, refactor

--- src/components/filters/boundaryFilter.js
+++ src/components/filters/boundaryFilter.js
@@ -4,30 +4,42 @@ import { reverseFormatTime } from "../../utils/timeAndDate";
 const BoundaryFilter = ({ setFilterCriteria, criteriaKey }) => {
   const [operator, setOperator] = useState("greater than");
   const [defaultValue, setDefaultValue] = useState(-Infinity);
+  const [filterValue, setFilterValue] = useState("");
 
   useEffect(() => {
     setDefaultValue(operator === "greater than" ? -Infinity : Infinity);
   }, [operator]);
 
-  const handleChange = (event) => {
-    const newValue = event.target.value ? event.target.value : defaultValue;
+  useEffect(() => {
+    updateFilterCriteria(operator, filterValue);
+  }, [filterValue]);
+
+  const updateFilterCriteria = (operator, filterValue) => {
     setFilterCriteria((prevCriteria) => {
       return {
         ...prevCriteria,
         [criteriaKey]: {
-          value: reverseFormatTime(newValue, defaultValue),
+          value: reverseFormatTime(filterValue, defaultValue),
           operator,
         },
       };
     });
   };
 
+  const handleChange = (operator) => {
+    setOperator(operator);
+    updateFilterCriteria(operator, filterValue);
+  };
+
   return (
     <div className="filter boundary">
-      <input id="boundary-filter" type="text" onChange={handleChange} />
+      <input
+        id="boundary-filter"
+        type="text"
+        onChange={(e) => setFilterValue(e.target.value)}
+      />
       <label htmlFor="boundary-filter">{criteriaKey}</label>
-      {/* TODO: Make filter criteria update on operator change */}
-      <select value={operator} onChange={(e) => setOperator(e.target.value)}>
+      <select value={operator} onChange={(e) => handleChange(e.target.value)}>
         <option value="greater than">Greater than</option>
         <option value="less than">Less than</option>
         <option value="equal to">Equal to</option>
