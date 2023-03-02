bad52bae43e87b9663ee26eeab16ebb2e1198ce6
execut4ble <jokubas.banaitis@gmail.com>
2023-03-02 21:02:32 +0200

    Update servers component

--- src/components/servers.js
+++ src/components/servers.js
@@ -17,13 +17,14 @@ const ServerList = () => {
   return (
     <ul style={{ listStyle: `none`, margin: `0 auto` }}>
       <Server country="se" address="play.sourceruns.org:27016"></Server>
-      <Server country="uk" address="212.71.238.124:27015"></Server>
-      <Server country="us" address="173.255.224.173:27015"></Server>
-      <Server country="de" address="139.162.187.16:27015"></Server>
+      <Server country="uk" address="hlkz.openag.pro"></Server>
+      <Server country="us" address="155.138.236.245"></Server>
+      <Server country="de" address="139.162.187.16"></Server>
       <Server country="fr" address="45.76.46.156:27016" desc="100fps"></Server>
-      <Server country="fr" address="45.76.46.156:27015"></Server>
+      <Server country="br" address="216.238.105.134"></Server>
       <Server country="nl" address="108.61.99.176:27016"></Server>
-      <Server country="de" address="80.240.26.43:27015"></Server>
+      <Server country="fr" address="45.76.46.156"></Server>
+      <Server country="de" address="80.240.26.43"></Server>
     </ul>
   );
 };
